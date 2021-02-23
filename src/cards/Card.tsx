1850f570d517ad090db150ae674513433c222ff0
Valentina Mackovic <mackovicvalentina@gmail.com>
2021-02-23 22:14:24 +0100

    Add card components and bootstrap icons

---
+++ src/cards/Card.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="bg-secondary rounded m-1 px-2 py-1 d-flex justify-content-between">
+      A card
+      <i className="bi bi-pencil-fill" />
+    </div>
+  );
+}
+
+export default Card;
--- src/kanbanBoardView/List.tsx
+++ src/kanbanBoardView/List.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Card from "../cards/Card";
 
 interface ListProps {
   list: List;
@@ -7,7 +8,11 @@ interface ListProps {
 function List({ list }: ListProps) {
   return (
     <div className="bg-dark rounded kanban-column-width p-2">
-      <div>{list.name}</div>
+      <div className="list-title p-1">{list.name}</div>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
